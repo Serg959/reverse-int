f2543ef792753e53acd0162566cb6de6163b6833
Serg959 <7703394@gmail.com>
2022-02-22 09:57:55 +0300

    solv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n);
+  let strNew = '';
+  if ((str[0] == '-') || (str[0] == '+')) {
+        for (let i = 1; i < str.length; i++) {
+            strNew = str[i] + strNew;
+         } 
+    return +strNew;
+    }
+    else {
+        for (let i = 0; i < str.length; i++) {
+            strNew = str[i] + strNew;
+         } 
+    return +strNew;  
+    }  
 }
